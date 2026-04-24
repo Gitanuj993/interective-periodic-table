20cea4c454a4dc867625cf499bb98197c89c7380
Gitanuj993 <anujtawar42@gmail.com>
2026-04-24 15:37:44 +0530

    t1.py

---
+++ tests/t1.py
@@ -0,0 +1,38 @@
+""""
+
+valency , periodic trends, 
+transition elements , 
+ionic, covalent and 
+
+
+
+
+
+
+
+"""
+# CLI program to take two elements from the modern periodic table and return compound made from them
+
+#structure
+metals = ["Na","Li","Be","Bo",]
+non_metals = ["Cl","C","O","Ni"]
+inert_gases   = [ "Ne","Ar"]
+
+# valency of sodium is 1 
+# valency of chlorine is -1 
+# they wil made a ionic bond which creates NaCl
+
+#structure 2
+metal_dict = { "name" : "Nitrogen","atomic_no":  11,"valency" : +1 } # group, catogiery
+
+ch_element1 = "Na"
+ch_element2 = "Cl" 
+
+#function exception handling , files 
+new_compound = ch_element1 + ch_element2
+
+print(f"your New Compound is : {new_compound}")
+
+
+
+
